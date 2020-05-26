a8e8954ab041b22234f1f846d91b9e8057f62642
Pazso <pzsolti99@gmail.com>
2020-05-27 00:59:40 +0200

    made template footer

---
+++ include/footer.php
@@ -0,0 +1,21 @@
+<footer class="footer">
+    <div class="footer-container">
+        <div class="footer-logo">
+            <a href="index.php"><img src="./img/logo.png" alt=""></a>
+            <p>Copyright &copy; 2020 DailyCalories</p>
+        </div>
+        <div class="footer-info">
+            <h3>Hello</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe quae explicabo voluptates eveniet blanditiis veritatis recusandae ducimus quisquam nesciunt iusto.</p>
+        </div>
+        <div class="footer-links">
+            <h3>Links</h3>
+            <a href="">Home</a>
+            <a href="">DailyCalorie</a>
+            <a href="">Recipes</a>
+            <a href="">Workouts</a>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
