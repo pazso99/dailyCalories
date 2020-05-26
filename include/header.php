c83d93bbdb09c471b3aa937a7ba109caadd29849
Pazso <pzsolti99@gmail.com>
2020-05-26 16:54:36 +0200

    linked script

--- include/header.php
+++ include/header.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="css/style.css">
     <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
     <script src="script/responsive.js" defer></script>
+    <script src="script/search.js" defer></script>
 </head>
 
 <body>
